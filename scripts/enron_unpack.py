5109fe2c7022c169e4398c342a6e5ca58d160d35
gitlinkberkeley <geoffrey.link@gmail.com>
2016-07-22 18:56:25 -0400

    Updated to include only text_ directories

--- scripts/enron_unpack.py
+++ scripts/enron_unpack.py
@@ -1,10 +1,16 @@
 import zipfile
 import os
+import sys
 
 for root, directories, filenames in os.walk('/enron/edrm-enron-v2/'):
 	for f in filenames:
 		if f.find('xml.zip') > 0:
-			zf = zipfile.ZipFile(root + f)
-			for z in zf.namelist():
-				if z.find('.txt') > 0:
-					os.system("unzip " + root + f + " " + z + " -d /enron_output/")
+			try:
+				zf = zipfile.ZipFile(root + f)
+				for z in zf.namelist():
+					if z.find('.txt') > 0:
+						if z.find('text_') == 0:
+							print "unzip " + root + f + " " + z + " -d /enron_output/"
+#							os.system("unzip " + root + f + " " + z + " -d /enron_output/")
+			except:
+				print (sys.exc_info()[0])
