6ecb43723dfe3d6b7d181ada3fe8f86f1ea7dc95
gitlinkberkeley <geoffrey.link@gmail.com>
2016-07-23 08:50:55 -0400

    Modified to allow submission of letter for parallel execution
    python enron_unpack.py a
    python enron_unpack.py b
    ...
    python enron_unpack.py z

--- scripts/enron_unpack.py
+++ scripts/enron_unpack.py
@@ -4,12 +4,13 @@ import sys
 
 for root, directories, filenames in os.walk('/enron/edrm-enron-v2/'):
 	for f in filenames:
-		if f.find('xml.zip') > 0:
-			try:
-				zf = zipfile.ZipFile(root + f)
-				for z in zf.namelist():
-					if z.find('.txt') > 0:
-						if z.find('text_') == 0:
-							os.system("unzip " + root + f + " " + z + " -d /enron_output/")
-			except:
-				print (sys.exc_info()[0])
+		if f[:15] == 'edrm-enron-v2_' + sys.argv[1]:
+			if f.find('xml.zip') > 0:
+				try:
+					zf = zipfile.ZipFile(root + f)
+					for z in zf.namelist():
+						if z.find('.txt') > 0:
+							if z.find('text_') == 0:
+								os.system("unzip " + root + f + " " + z + " -d /enron_output/")
+				except:
+					print (sys.exc_info()[0])
