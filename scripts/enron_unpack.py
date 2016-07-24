96cc9948608c6cd20b1a27d6ab2cdf6ed55373c1
mpera <megan.pera@gmail.com>
2016-07-24 10:54:03 -0700

    Additional comments to make code more clear

--- scripts/enron_unpack.py
+++ scripts/enron_unpack.py
@@ -1,4 +1,7 @@
+# Data are stored as .zip files for each Enron employee. This file pulls out the .txt files from each .zip file. 
+
 # Execute in parallel 26 times using the letters of the alphabet (matched to last name of person)
+# To execute, run : python enron_unpack.py <letter of alphabet>
 # Execution time parallel 26 times on a m3.large machine is about 12 hours
 # Best to kick it off a few hours before bedtime for fresh data in the morning
 # python enron_unpack.py a
@@ -10,15 +13,17 @@ import zipfile
 import os
 import sys
 
-for root, directories, filenames in os.walk('/enron/edrm-enron-v2/'):
+for root, directories, filenames in os.walk('/enron/edrm-enron-v2/'): 
 	for f in filenames:
-		if f[:15] == 'edrm-enron-v2_' + sys.argv[1]:
+		if f[:15] == 'edrm-enron-v2_' + sys.argv[1]: # allows user to specify letter from alphabet
 			if f.find('xml.zip') > 0:
+				# try/except statement bypasses any "bad zipfile" errors
 				try:
 					zf = zipfile.ZipFile(root + f)
 					for z in zf.namelist():
 						if z.find('.txt') > 0:
 							if z.find('text_') == 0:
+								# save .txt files to /enron_output/text_00* folders
 								os.system("unzip " + root + f + " " + z + " -d /enron_output/")
 				except:
 					print (sys.exc_info()[0])
