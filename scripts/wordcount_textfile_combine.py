ee868819f5905d0d89d9824a73353ef18d1267fc
gitlinkberkeley <geoffrey.link@gmail.com>
2016-07-24 08:09:11 -0400

    Updated Comments

--- scripts/wordcount_textfile_combine.py
+++ scripts/wordcount_textfile_combine.py
@@ -1,8 +1,8 @@
 #
-# Aggregates all email text files into a single 11Gb file
+# Aggregates all email text files into a single 8.3Gb file
 # Monitor file size while generating from the command line using: du -hs /enron_output/enron_emails_text_all.txt
 # Upload to Hadoop using the command: hdfs dfs -put /enron_output/enron_emails_text_all.txt /user/w205/enron_emails_text_all.txt
-# Appoximate time to generate and upload to Hadoop is 30 minutes using a m3.medium host
+# Appoximate time to generate and upload to Hadoop is 30 minutes using a m3.large host
 #
 
 import os
@@ -15,22 +15,7 @@ paths = (
                 '/enron_output/text_004',
                 '/enron_output/text_005',
                 '/enron_output/text_006',
-                '/enron_output/text_007',
-                '/enron_output/text_008',
-                '/enron_output/text_009',
-                '/enron_output/text_010',
-                '/enron_output/text_011',
-                '/enron_output/text_012',
-                '/enron_output/text_013',
-                '/enron_output/text_014',
-                '/enron_output/text_015',
-                '/enron_output/text_016',
-                '/enron_output/text_017',
-                '/enron_output/text_018',
-                '/enron_output/text_019',
-                '/enron_output/text_020',
-                '/enron_output/text_021',
-                '/enron_output/text_022'
+                '/enron_output/text_007'
 	)
 
 with open ('/enron_output/enron_emails_text_all.txt', 'w') as outfile:
@@ -43,3 +28,4 @@ with open ('/enron_output/enron_emails_text_all.txt', 'w') as outfile:
 				print pf
 				with open(pf, 'r') as infile:
 					outfile.write(infile.read())
+
