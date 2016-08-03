cebf550195017960c3232ff6c1f05ebab47548ea
jablauvelt <jblauvelt8@gmail.com>
2016-08-03 16:22:44 -0500

    Update 3_setup_postgres_db.sh

--- scripts/1_setup/3_setup_postgres_db.sh
+++ scripts/1_setup/3_setup_postgres_db.sh
@@ -1,1 +1,16 @@
-#should contain the bash commands to set up the postgres DB (the script to create the tables can be separate).
+#!/bin/bash
+
+### POSTGRES
+# Setup folders
+mkdir /data/pgsql
+mkdir /data/pgsql/data
+mkdir /data/pgsql/logs
+chown -R postgres /data/pgsql
+sudo -u postgres initdb -D /data/pgsql/data
+
+#setup pg_hba.conf
+sudo -u postgres echo "host    all         all         0.0.0.0         0.0.0.0               md5" >> /data/pgsql/data/pg_hba.conf
+
+#setup postgresql.conf
+sudo -u postgres echo "listen_addresses = '*'" >> /data/pgsql/data/postgresql.conf
+sudo -u postgres echo "standard_conforming_strings = off" >> /data/pgsql/data/postgresql.conf
