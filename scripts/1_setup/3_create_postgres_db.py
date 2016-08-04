86d2bf0265c748c9a69b38af7a6144d8fc97cf89
mpera <megan.pera@gmail.com>
2016-08-03 21:31:20 -0500

    Script to create finalproject database in postgres

---
+++ scripts/1_setup/3_create_postgres_db.py
@@ -0,0 +1,10 @@
+# Creating finalproject database on postgres
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+cur = conn.cursor()
+cur.execute("CREATE DATABASE finalproject")
