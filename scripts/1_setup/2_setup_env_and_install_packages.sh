7ef87eb33df8cfdd1af9e3cd40edd73c073a7160
mpera <megan.pera@gmail.com>
2016-08-08 09:54:16 -0500

    Update 2_setup_env_and_install_packages.sh

--- scripts/1_setup/2_setup_env_and_install_packages.sh
+++ scripts/1_setup/2_setup_env_and_install_packages.sh
@@ -8,17 +8,3 @@ wget https://s3.amazonaws.com/ucbdatasciencew205/setup_ucb_complete_plus_postgre
 chmod +x ./setup_ucb_complete_plus_postgres.sh
 # Takes ~6 minutes to complete
 ./setup_ucb_complete_plus_postgres.sh /dev/xvdf #CHANGE based on path to 100G volume, from above 
-
-# Mount EBS volumes on the instance
-# First, attach enron volume on aws
-# Change paths based on where the volumes are
-fdisk -l
-mkdir /enron
-mount -t ext4 /dev/xvdf /enron
-mount -t ext4 /dev/xvdg /data
-
-# Create a folder for output data
-mkdir /enron_output
-
-
-# Write all outputs to the enron_output folder
