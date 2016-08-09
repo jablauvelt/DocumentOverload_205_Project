6567e6d9cc54c5782bc89bc9c84f7e5d5d5e384a
jablauvelt <jblauvelt8@gmail.com>
2016-08-09 10:52:58 -0500

    Update 2_setup_env_and_install_packages.sh

--- scripts/1_setup/2_setup_env_and_install_packages.sh
+++ scripts/1_setup/2_setup_env_and_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#!!!!!!!!!!!!!!!!chmod a+rwx /enron_output # makes the data folder rwx to all users
+#!! do we need this? --> chmod a+rwx /enron_output # makes the data folder rwx to all users
 
 # Setup folders
 mkdir /enron_output/pgsql
@@ -22,22 +22,11 @@ sudo -u postgres pg_ctl -D /enron_output/pgsql/data -l /enron_output/pgsql/logs/
 # Check that it's running:
 ps auxwww | grep postgres
 
-
-# Python 2.7 install - since the Amazon instance comes with Python 2.6
-sudo yum install python27-devel –y
-# Check current system Python version
-python --version
-# --> Python 2.6.6 [for example]
-# Rename the current version to reflect its correct version
-mv /usr/bin/python /usr/bin/python266
-# Create a symbolic link from the file in the path to the version you want to execute
-ln -s /usr/bin/python2.7 /usr/bin/python
-# Check that the shell picks up the version of Python you intended
-python --version
-#--> Python 2.7.3
-
+# Install python packages
+pip install pymongo
 
 # Pull in scripts from GitHub Repository (this one), so you can run scripts from the command line
-`cd /home/w205`
-`git clone https://jablauvelt@github.com/jablauvelt/DocumentOverload_205_Project.git`
-`cd /DocumentOverload_205_Project/scripts`
+cd /home/w205
+git clone https://jablauvelt@github.com/jablauvelt/DocumentOverload_205_Project.git
+cd /DocumentOverload_205_Project/scripts
+
