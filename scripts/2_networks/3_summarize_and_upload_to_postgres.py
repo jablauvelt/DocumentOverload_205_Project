9a687156538f19bed489bcd148acc3865dcb356a
root <root@ip-172-31-2-206.ec2.internal>
2016-08-12 17:21:27 +0000

    YES finally got the aggregation working in pymongo

--- scripts/2_networks/3_summarize_and_upload_to_postgres.py
+++ scripts/2_networks/3_summarize_and_upload_to_postgres.py
@@ -1,1 +1,24 @@
-pprint(xmls.find_one()['Tags']
+### Document Overload
+
+import pymongo
+from pprint import pprint
+
+# Access MongoDB "xmls" collection
+client = pymongo.MongoClient()
+db = client.xml_db
+collection = db.xmls
+xmls = collection.xmls
+tags = collection.tags
+
+# Aggregate froms
+froms = list(xmls.aggregate([{'$unwind': '$Document.Tags.Tag'},
+                       {'$match': {'Document.Tags.Tag.@TagName': '#From'}},
+                       {'$group': {'_id': '$Document.@DocID', '#From': {'$first': '$Document.Tags.Tag.@TagValue'}}}], allowDiskUse=True))
+
+# Aggregate tos
+tos = list(xmls.aggregate([{'$unwind': '$Document.Tags.Tag'},
+                       {'$match': {'Document.Tags.Tag.@TagName': '#To'}},
+                       {'$group': {'_id': '$Document.@DocID', '#To': {'$first': '$Document.Tags.Tag.@TagValue'}}}], allowDiskUse=True))
+
+
+
