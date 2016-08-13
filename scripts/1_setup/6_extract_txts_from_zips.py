2ea12a4471a479fa6425d7e916bf005a786c6a7e
gitlinkberkeley <geoffrey.link@gmail.com>
2016-08-13 15:52:45 -0400

    Updated to write from /enron to /enron_outpu
    Should make things a little faster

--- scripts/1_setup/6_extract_txts_from_zips.py
+++ scripts/1_setup/6_extract_txts_from_zips.py
@@ -1,6 +1,6 @@
-# Run in /enron_output/staging directory
+# Extract to /enron/staging directory (next script will read from /enron/staging and write to /enron_output; hence, less I/O on a single mount)
 # To run : $ python 6_extract_txts_from_zips.py
-# Monitor progress with  $ find /enron_output/staging | wc -l
+# Monitor progress with  $ find /enron/staging | wc -l
 # Takes ~5 minutes to run
 # The final product should have ~1,720,792 files
 
@@ -11,5 +11,5 @@ for root, directories, filenames in os.walk('/enron/edrm-enron-v2/'):
 		if f.find('xml.zip') > 0:
 			filename = root + f
 			lastname = filename[filename.index('_')+1:][:filename[filename.index('_')+1:].index('_')]
-			print("unzip -qq -j " + filename + " \"*.txt\" -x \"native*\" -d /enron_output/staging/staging_" + lastname + " &")
-			os.system("unzip -qq -j " + filename + " \"*.txt\" -x \"native*\" -d /enron_output/staging/staging_" + lastname + " &")
+			print("unzip -qq -j " + filename + " \"*.txt\" -x \"native*\" -d /enron/staging/staging_" + lastname + " &")
+			os.system("unzip -qq -j " + filename + " \"*.txt\" -x \"native*\" -d /enron/staging/staging_" + lastname + " &")
--- scripts/1_setup/7_combine_txts.py
+++ scripts/1_setup/7_combine_txts.py
@@ -1,17 +1,19 @@
-#
 # Aggregates all email text files into a single 11Gb file
 # Monitor file size while generating from the command line using: du -hs /enron_output/enron_emails_text_all.txt
 # Appoximate time to generate is 30 minutes using a m3.medium host
-#
+# I/O waits are clearly the most significant issue with this script; hence, use buffering, read from one mount, write to other mount
 
 import os
+counter = 0
 
 with open ('/enron_output/enron_emails_text_all.txt', 'w') as outfile:
-
-        for root, directories, filenames in os.walk('/enron_output/staging'):
+        for root, directories, filenames in os.walk('/enron/staging'):
                 for f in filenames:
                         pf = os.path.join(root, f)
-                        print pf
-                        with open(pf, 'r') as infile:
+                        counter = counter + 1
+                        if counter % 10000 == 0:
+                            print "Files Merged: " + str(counter)
+                        with open(pf, 'r', 1000000) as infile:
                                 outfile.write(infile.read())
-
+                        infile.close()
+outfile.close()
