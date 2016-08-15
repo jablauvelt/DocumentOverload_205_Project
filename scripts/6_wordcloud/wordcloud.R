6f2dbb51891ac5083c3cd3442694e4e7559433d9
mpera <megan.pera@gmail.com>
2016-08-15 13:40:04 -0500

    Delete wordcloud.R

--- scripts/6_wordcloud/wordcloud.R
+++
@@ -1,9 +0,0 @@
-## Installing necessary packages
-install.packages("wordcloud")
-library(wordcloud)
-
-## Importing data
-wordcount = read.csv("~/Desktop/wordcloud/wordcount.csv", header=TRUE)
-
-## Creating the wordcloud
-wordcloud(wordcount$text,wordcount$frequency,scale=c(4,.5),min.freq=3,max.words=100, random.color=TRUE, rot.per=.1,ordered.colors=FALSE,use.r.layout=FALSE, fixed.asp=TRUE)
